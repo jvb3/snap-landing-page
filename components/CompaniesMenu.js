cb001ff4654e1ee20305beafa647314d01fb93b2
jvb3 <92876789+jvb3@users.noreply.github.com>
2022-07-19 15:03:02 +0900

    Add files via upload

---
+++ components/CompaniesMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CompaniesMenu() {
+  return (
+    <div className="m-nav__companies-menu">
+      <span>History</span>
+      <span>Our Team</span>
+      <span>Blog</span>
+    </div>
+  );
+}
+
+export default CompaniesMenu;
---
+++ components/MobileNavLinks.js
@@ -0,0 +1,83 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import FeaturesMenu from "./FeaturesMenu";
+import CompaniesMenu from "./CompaniesMenu";
+
+function MobileNavLinks({
+  closeMenu,
+  setCloseMenu,
+  showMobileMenu,
+  setShowMobileMenu,
+  showFeaturesMenu,
+  setShowFeaturesMenu,
+  showCompaniesMenu,
+  setShowCompaniesMenu,
+}) {
+  return (
+    <>
+      <ul className="m-nav">
+        {closeMenu && (
+          <img
+            onClick={() => {
+              setCloseMenu(false);
+              setShowMobileMenu(false);
+              setShowFeaturesMenu(false);
+              setShowCompaniesMenu(false);
+            }}
+            className="m-nav__close-button"
+            src="images/icon-close-menu.svg"
+            alt="close menu button"
+          />
+        )}
+
+        <div>
+          <li className="m-nav-link__title">
+            <span className="m-nav__features-title">Features</span>
+            {showFeaturesMenu ? (
+              <img
+                onClick={() => setShowFeaturesMenu(false)}
+                className="m-nav-link__icon"
+                src="images/icon-arrow-up.svg"
+              />
+            ) : (
+              <img
+                onClick={() => setShowFeaturesMenu(true)}
+                className="m-nav-link__icon"
+                src="images/icon-arrow-down.svg"
+              />
+            )}
+            {showFeaturesMenu && <FeaturesMenu />}
+          </li>
+        </div>
+
+        <div>
+          <li className="m-nav-link__title">
+            <span className="m-nav__companies-title">Company</span>
+            {showCompaniesMenu ? (
+              <img
+                onClick={() => setShowCompaniesMenu(false)}
+                className="m-nav-link__icon"
+                src="images/icon-arrow-up.svg"
+              />
+            ) : (
+              <img
+                onClick={() => setShowCompaniesMenu(true)}
+                className="m-nav-link__icon"
+                src="images/icon-arrow-down.svg"
+              />
+            )}
+            {showCompaniesMenu && <CompaniesMenu />}
+          </li>
+        </div>
+        <li>Careers</li>
+        <li>About</li>
+        <div className="m-nav__buttons">
+          <button className="m-nav__login-button">Login</button>
+          <button className="m-nav__reg-button">Register</button>
+        </div>
+      </ul>
+    </>
+  );
+}
+
+export default MobileNavLinks;
